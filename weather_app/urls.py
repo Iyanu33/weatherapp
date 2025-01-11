af75b343afd8296c798008138f2d565544ebbb17
Iyanu33 <olaneganiyanu@gmail.com>
2025-01-11 19:50:35 +0100

    nope! not me,scipy is dumb

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('weather.urls', 'weather'), namespace='core')),
     path('weather/', include('weather.urls')),
 ]
