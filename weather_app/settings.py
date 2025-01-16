acb7cec410361cf09373f8d4cc206e5665bced8a
Iyanu33 <olaneganiyanu@gmail.com>
2025-01-16 21:43:10 +0100

    let see

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j658boa(dh)ck6&!b&oazbhse5_ef)h($17l1#^!+#*=$j@ups'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
